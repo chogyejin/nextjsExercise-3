4c17a7efd6b338ef13a77d574b305682d3081e2f
chogyejin <whrpwls96@naver.com>
2022-02-05 23:48:49 +0900

    feat: add Link component in NavBar

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,3 +1,10 @@
+import NavBar from "../components/NavBar";
+
 export default function About() {
-  return "about us";
+  return (
+    <div>
+      <NavBar />
+      <h1>About us</h1>
+    </div>
+  );
 }
