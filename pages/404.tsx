a02b9e03babf12f229ed6db464488a37db483d3a
chogyejin <whrpwls96@naver.com>
2022-02-11 21:31:33 +0900

    feat: customize 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,18 @@
+export default function NotFound() {
+  return (
+    <>
+      <div className="title">
+        <div>Not Found</div>
+      </div>
+      <style jsx>{`
+        .title {
+          display: flex;
+          height: 300px;
+          justify-content: center;
+          align-items: center;
+          font-size: 50px;
+        }
+      `}</style>
+    </>
+  );
+}
