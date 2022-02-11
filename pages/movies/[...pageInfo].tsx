52bb0ccdcd7d075d036d2f0884c1458e147b68f2
chogyejin <whrpwls96@naver.com>
2022-02-11 23:38:02 +0900

    feat: add movie detail page with rewritten api url

--- pages/movies/[...pageInfo].tsx
+++ pages/movies/[...pageInfo].tsx
@@ -1,6 +1,8 @@
 import { GetServerSidePropsContext } from "next";
 import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import Seo from "../../components/Seo";
+import axios from "axios";
 
 interface DetailProps {
   params: string[];
@@ -15,11 +17,19 @@ export default function Detail({ params }: DetailProps) {
   // getServerSideProps 사용하여 params 받아올 때
   const [title, id] = params;
 
+  useEffect(() => {
+    (async () => {
+      const result = await axios.get(`/api/movies/${id}`);
+      console.log(result.data);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title={title}></Seo>
       <h4>{title}</h4>
       <h4>{id}</h4>
+      <h4>{}</h4>
     </div>
   );
 }
